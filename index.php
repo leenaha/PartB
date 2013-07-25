9cfcfedcbfc6a33e4e3d2e0141e4b86eaa052094
Leena Ha <s3237042@student.rmit.edu.au>
2013-07-25 13:43:24 +0000

    updated index.php

--- index.php
+++ index.php
@@ -2,27 +2,48 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Wine Search</title>
+        <title></title>
     </head>
     <body>
         <form>
-		<table>
-			<tr>
-				<td>Wine Name</td>
-				<td><input type="text" name="wine_name"></td>
-			</tr>
-			<tr>
-				<td>Winery Name</td>
-				<td><input type="text" name="winery_name"></td>
-			</tr>
-			<tr>
-				<td>Region</td>
-				<td>
-				
-				</td>
-			</tr>
-		</table>
-	</form>
+            <table>
+                <tr>
+                    <td>Wine Name</td>
+                    <td><input type="text" name=""></td>
+                </tr>    
+                    <td>Winery Name</td>
+                    <td><input type="text" name=""></td>
+                <tr>
+                    <td>Region</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Grape</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Year</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Minimum number of wines in stock, per wine</td>
+                    <td><input type="text" name=""></td>
+                </tr>
+                <tr>
+                    <td>Minimum number of wines ordered, per wine</td>
+                    <td><input type="text" name=""></td>
+                </tr>
+                <tr>
+                    <td>Min Cost</td>
+                    <td><input type="text" name=""></td>
+                </tr>
+                <tr>
+                    <td>Max Cost</td>
+                    <td><input type="text" name=""></td>
+                </tr>
+            </table>
+        </form>
     </body>
 </html>
+
 
