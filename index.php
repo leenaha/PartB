c3aa90e879093ddc72dbd883fc747275aa861ba7
Leena Ha <s3237042@student.rmit.edu.au>
2013-07-25 08:42:26 +0000

    updated index.php by addimg table

--- index.php
+++ index.php
@@ -5,9 +5,24 @@
         <title>Wine Search</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form>
+		<table>
+			<tr>
+				<td>Wine Name</td>
+				<td><input type="text" name="wine_name"></td>
+			</tr>
+			<tr>
+				<td>Winery Name</td>
+				<td><input type="text" name="winery_name"></td>
+			</tr>
+			<tr>
+				<td>Region</td>
+				<td>
+				
+				</td>
+			</tr>
+		</table>
+	</form>
     </body>
 </html>
 
