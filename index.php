178b45e8d3d89f71eb56dcd2ac599c1f324426bd
Leena Ha <s3237042@student.rmit.edu.au>
2013-07-25 14:52:54 +0000

    index.php with bootstrap

--- index.php
+++ index.php
@@ -1,49 +1,96 @@
-<!DOCCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Wine Search</title>
+        
+        <!-- Le styles -->
+        <link href="bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet">
+        <style>
+            .table th, .table td {
+                border-top:0px;
+            }
+        </style>
     </head>
     <body>
-        <form>
-            <table>
-                <tr>
-                    <td>Wine Name</td>
-                    <td><input type="text" name=""></td>
-                </tr>    
-                    <td>Winery Name</td>
-                    <td><input type="text" name=""></td>
-                <tr>
-                    <td>Region</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Grape</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Year</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Minimum number of wines in stock, per wine</td>
-                    <td><input type="text" name=""></td>
-                </tr>
-                <tr>
-                    <td>Minimum number of wines ordered, per wine</td>
-                    <td><input type="text" name=""></td>
-                </tr>
-                <tr>
-                    <td>Min Cost</td>
-                    <td><input type="text" name=""></td>
-                </tr>
-                <tr>
-                    <td>Max Cost</td>
-                    <td><input type="text" name=""></td>
-                </tr>
-            </table>
-        </form>
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="alert alert-info"><i class='icon-search'></i> <b>Wine Search Filter</b></div>
+
+                <form class="form-horizontal well" style='width:480px;'>
+
+                    <div class="control-group">
+                      <label class="control-label" for="wineName">Name of Wine</label>
+                      <div class="controls">
+                        <input type="text" id="wineName" placeholder="Wine Name">
+                      </div>
+                    </div>
+
+                    <div class="control-group">
+                      <label class="control-label" for="wineryName">Name of Winery</label>
+                      <div class="controls">
+                        <input type="text" id="wineryName" placeholder="Winery Name">
+                      </div>
+                    </div>
+
+                    <div class="control-group">
+                      <label class="control-label" for="wineryName">Region</label>
+                      <div class="controls">
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="control-group">
+                      <label class="control-label" for="wineryName">Years</label>
+                      <div class="controls">
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="control-group">
+                      <label class="control-label" for="minWineInStock">Minimum no. of wines in stock</label>
+                      <div class="controls">
+                        <input type="text" id="minWineInStock" placeholder="" class="input-small">
+                      </div>
+                    </div>
+
+                    <div class="control-group">
+                      <label class="control-label" for="minWineInStock">Minimum no. of wines orders</label>
+                      <div class="controls">
+                        <input type="text" id="minWineOrdered" placeholder="" class="input-small">
+                      </div>
+                    </div>
+
+                    <div class="control-group">
+                      <label class="control-label" for="minPrice">Min Price</label>  
+                      <div class="controls">
+                      <input type="text" id="minPrice" placeholder="" class="input-small">
+                      - Max Price
+                      <input type="text" id="maxPrice" placeholder="" class="input-small">
+                      </div>
+                    </div>
+
+                    <div class="control-group">
+                        <div class="controls">
+                            <button type="submit" class="btn btn-success">Search</button>
+                        </div>
+                    </div>
+                </form>
+                </div><!--closes span6-->
+            
+        </div><!-- closes row fluid -->
     </body>
 </html>
-
 
