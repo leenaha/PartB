128219bf095bc5f632373d44bbc47a9ee14499db
Leena Ha <s3237042@student.rmit.edu.au>
2013-08-01 03:05:04 +0000

    PART A - updated index.php with SQL

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+    
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,12 +13,31 @@
             }
         </style>
     </head>
+    
+    <?php
+	require_once('db.php');
+	if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
+      		echo 'Could not connect to mysql on ' . DB_HOST . '\n';
+      		exit;
+   	}
+	
+	if(!mysql_select_db(DB_NAME, $dbconn)) {
+      		echo 'Could not user database ' . DB_NAME . '\n';
+     	 	echo mysql_error() . '\n';
+      		exit;
+   	}
+    ?>
+    
+    
     <body>
         <div class="row-fluid">
             <div class="span12">
+            <div class="container" style="width:550px;">
+           
                 <div class="alert alert-info"><i class='icon-search'></i> <b>Wine Search Filter</b></div>
-
-                <form class="form-horizontal well">
+                
+                
+                <form class="form-horizontal well" action="process.php" method="get">
 
                     <div class="control-group">
                       <label class="control-label" for="wineName">Name of Wine</label>
@@ -34,28 +54,71 @@
                     </div>
 
                     <div class="control-group">
-                      <label class="control-label" for="wineryName">Region</label>
+                      <label class="control-label" for="grapeVariety">Grape Variety</label>
                       <div class="controls">
                         <select>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
+                            <?php 
+                                $query = "SELECT variety_id, variety 
+                                            FROM grape_variety";
+                                $result = mysql_query($query);
+                                while($query_data = mysql_fetch_array($result)){
+                                    $variety_id = $query_data["variety_id"];
+                                    $variety_name = $query_data["variety"];
+                                    
+                                    echo "<option value={$variety_id}>{$variety_name}</option>";
+                                } 
+                             ?>
+                        </select>
+                      </div>
+                    </div>
+                    
+                    <div class="control-group">
+                      <label class="control-label" for="regionName">Region</label>
+                      <div class="controls">
+                        <select>
+                            <?php 
+                                $query = "SELECT region_name, region_id 
+                                            FROM region";
+                                $result = mysql_query($query);
+                                while($query_data = mysql_fetch_array($result)){
+                                    $region_id = $query_data["region_id"];
+                                    $region_name = $query_data["region_name"];
+                                    
+                                    echo "<option value={$region_id}>{$region_name}</option>";
+                                } 
+                             ?>
                         </select>
                       </div>
                     </div>
 
                     <div class="control-group">
-                      <label class="control-label" for="wineryName">Years</label>
+                      <label class="control-label" for="minYears">Minimum of Years</label>
                       <div class="controls">
-                        <select>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
+                            <select class="input-small" id="minYears">
+                            <?php 
+                                $query = "SELECT DISTINCT year
+                                            FROM wine
+                                            ORDER BY  wine.year ASC";
+                                $result = mysql_query($query);
+                                while($query_data = mysql_fetch_array($result)){
+                                    $year = $query_data["year"];    
+                                    echo "<option>{$year}</option>";
+                                } 
+                             ?>
+                            </select>
+                            - Maximum Years 
+                            <select class="input-small" id="maxYears">
+                            <?php 
+                                $query = "SELECT DISTINCT year
+                                            FROM wine
+                                            ORDER BY  wine.year DESC";
+                                $result = mysql_query($query);
+                                while($query_data = mysql_fetch_array($result)){
+                                    $year = $query_data["year"];    
+                                    echo "<option>{$year}</option>";
+                                } 
+                             ?>
+                            </select>
                       </div>
                     </div>
 
@@ -88,9 +151,9 @@
                         </div>
                     </div>
                 </form>
-                </div><!--closes span6-->
-            
+             </div> 
         </div><!-- closes row fluid -->
+      </div>
     </body>
 </html>
 
